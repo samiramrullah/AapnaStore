f7d6edeb113e938913ba897662bf2db9eed8d166
samiramrullah <samiramrullah@gmail.com>
2023-11-03 17:28:33 +0530

    user route added

--- backend/api/Routes/UserManagement/user.js
+++ backend/api/Routes/UserManagement/user.js
@@ -1,14 +1,55 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose')
+const userSchema = require('../../../Model/UserManagement/user');
 const checkauth = require('../../middleware/check-auth')
 
 
+router.get('/getallusers', checkauth, async (req, res, next) => {
+    try {
+        const users = await userSchema.find({}, 'name email').exec();
+
+        if (users) {
+            res.status(200).json({
+                status: true,
+                users: users
+            });
+        } else {
+            res.status(404).json({
+                status: false,
+                message: 'No users found'
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: 'Error in getting users'
+        });
+    }
+});
+
+router.get("/:id", checkauth, async (req, res, next) => {
+    try {
+        const userId = req.params.id;
+        const user = await userSchema.findOne({ _id: userId} ,'name email').exec();
+        if (user) {
+            res.status(200).json({
+                status: true,
+                user,
+            });
+        } else {
+            res.status(404).json({
+                status: false,
+                message: 'User not found'
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: 'Error in getting user'
+        });
+    }
+});
 
 
-router.get('/getalluser', checkauth, (req, res, next) => {
-    res.status(200).json({
-        message: "all users",
-    })
-})
-
-module.exports=router;
+module.exports = router;
--- backend/api/middleware/check-auth.js
+++ backend/api/middleware/check-auth.js
@@ -2,7 +2,8 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
-        const decoded = jwt.verify(token, process.env.JWT_TOKEN_SECRET_KEY);   
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);   
+
         req.userData = decoded;
         
         next();
